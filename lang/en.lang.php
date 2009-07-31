798a92d8c7ad6f659fa5aaeb5d1f2be839bdd57e
haneul <syhan@cs.washington.edu>
2009-07-31 07:08:09 +0000

    English Translation
    git-svn-id: http://xe-textyle.googlecode.com/svn/sandbox/textyle@4 bebf4043-ad85-6e1f-322f-7830e3d0fd93

---
+++ lang/en.lang.php
@@ -0,0 +1,397 @@
+<?php
+    /**
+     * @file   en.lang.php
+     * @author haneul (haneul0318@gmail.com)
+     * @brief  Textyle(textyle) module's English language pack. 
+     **/
+
+    $lang->textyle = 'Textyle';
+    $lang->about_textyle = 'Textyle is a blog module running on XpressEngine.';
+
+	$lang->msg_create_textyle = 'Textyle is created';
+	$lang->init_category_title = 'Default Category';
+	$lang->add_category = 'Add a category';
+    $lang->textyle_admin = 'Textyle administrator(ID)';
+    $lang->textyle_title = 'Textyle\'s title';
+    $lang->today_visitor = 'Number of today\'s <strong>visitors</strong>';
+    $lang->today_comments = 'Number of today\'s <strong>comments</strong>';
+    $lang->today_trackbacks = 'Number of today\'s <strong>trackbacks/pingbacks</strong>';
+    $lang->textyle_summary = 'Summary of Textyle';
+    $lang->item_unit = ' ';
+    $lang->newest_documents ='Recent Posts';
+    $lang->newest_no_documents = "You have written nothing. Write some posts!"; 
+    $lang->newest_materials = 'Newest Ingredients';
+	$lang->newest_no_materials = 'There is no ingredient for writing. <a href="%s">Install a writing ingredients collector.</a>';
+	$lang->newest_comments = 'Recent Comments';
+	$lang->newest_no_comments = 'There is no comment.';
+	$lang->newest_guestbooks = 'Recent Guestbook';
+	$lang->newest_no_guestbooks = 'There is no article in guestbook.';
+	$lang->reserve = 'Reservation';
+    $lang->publish = 'Published';
+    $lang->temp = 'Temporary';
+    $lang->more = 'More';
+    $lang->last_week = 'Last week';
+    $lang->this_week = 'This week';
+    $lang->display_name = 'Display name';
+    $lang->about_display_name = 'If not entered, your ID will be displayed';
+    $lang->about_email = 'If not entered, you cannot find your password via email when you forget it.';
+    $lang->no_profile_image = 'There is no profile image';
+    $lang->allow_profile_image_type = '<strong>jpg, gif, png</strong> files can be uploade';
+    $lang->allow_profile_image_size = 'Image size will be adjusted into <strong>%dx%d</strong>px automatically.';
+    $lang->signature = 'Self Introduction';
+    $lang->default_config = 'Deafult Configuration';
+    $lang->blog_title = 'Blog\'s title';
+    $lang->about_blog_title = '';
+    $lang->blog_description = 'One-line Description';
+    $lang->about_blog_description = 'Whether to be displayed depends on skins, or you can show it by HTML&middot;CSS editor.';
+    $lang->favicon = 'Favicon';
+    $lang->registed_favicon = 'Registered Favicon';
+    $lang->about_favicon = '<strong>16x16px</strong> sized <strong>ico</strong> file can be uploaded.';
+    $lang->about_addon = 'Addons are to control Textyle\'s actions. Turning on/off addons would add useful features to your sites.';
+
+    $lang->lang_time_zone = 'Language/Time zone';
+    $lang->language = 'Language';
+    $lang->timezone = 'Time Zone';
+    $lang->edit_style = 'Select an Editor';
+    $lang->textyle_editor = 'Paragraph Editor';
+    $lang->font_style = 'Font style';
+    $lang->font_family = 'Font family';
+    $lang->font_size = 'Font size';
+    $lang->about_font_family = 'You can choose default fonts for contents (ex:%s)';
+    $lang->font_family_list = array(
+		'돋움, Dotum'=> 'Dotum',
+		'굴림, Gulim'=> 'Gulim',
+		'바탕, Batang'=> 'Batang',
+		'맑은 고딕, Malgun Gothic'=> 'Malgun Gothic',
+		'나눔고딕, NanumGothic'=> 'Nanum Gothic',
+		'나눔명조, NanumMyeongjo'=> 'Nanum Myeongjo'
+	);
+    $lang->about_font_size = 'You can choose default font size (ex: 12px or 1em - unit should be included)';
+    $lang->about_textyle_editor = 'Textyle\'s default editor: You can edit each paragraph separately in your posts';
+    $lang->etc_editor = 'Other Editors';
+    $lang->about_etc_editor = 'Rich text editors.';
+    $lang->set_prefix = 'Prefix';
+    $lang->about_prefix = 'Each post contains following content. (You can use HTML)';
+    $lang->set_suffix = 'Suffix';
+    $lang->about_suffix = 'Each post contains following content. (You can useHTML)';
+    $lang->blogapi = 'Blog API';
+    $lang->blogapi_support = 'Remote publishing via Blog API(Meta Weblog API) is available.';
+    $lang->blogapi_example = 'ex) Window Live Writer, Google Docs, MS Word 2007, etc.';
+    $lang->blogapi_url = 'API URL';
+    $lang->blog_first_page = 'First Page';
+    $lang->blog_display_target = 'Display target';
+    $lang->content_body = 'Contents';
+    $lang->content_summary = 'Summary';
+    $lang->content_list = 'List';
+    $lang->blog_display_count = 'Number of posts';
+    $lang->rss_type = '발행 대상';
+    $lang->rss_total = 'Title + All Contents';
+    $lang->rss_summary = 'Title + Summary';
+    $lang->visitor_editor_style = 'Input form for Comments and Guestbook';
+    $lang->host = 'Host';
+    $lang->referer = 'Referer';
+    $lang->link_word = 'Linked word';
+    $lang->link_document = 'Linked document';
+    $lang->visitor = 'Visitor';
+    $lang->about_referer = 'You can see visitors\' referer (the route they come through).';
+    $lang->website_address = 'Website Address';
+    $lang->required = 'Required';
+    $lang->selected = 'Optional';
+    $lang->notaccepted = 'Do not accept';
+    $lang->comment_editor = 'Comment editor';
+    $lang->guestbook_editor = 'Guestbook editor';
+    $lang->comment_list_count = 'Number of comments per page';
+    $lang->guestbook_list_count = 'Number of guestbook posts per page';
+    $lang->comment_grant = 'Permission for writing comments';
+    $lang->about_comment_grant = 'You may allow only members or all to write comments';
+    $lang->disable_comment= 'You\'are not allowed to write comments. Please sign in.';
+    $lang->grant_to_all = 'All';
+    $lang->grant_to_member = 'Members';
+    $lang->guestbook_grant = 'Permission for writing to guestbook';
+    $lang->about_guestbook_grant = 'You may allow only members or all to write posts to guestbook';
+    $lang->disable_guestbook = 'You\'are not allowed to write posts to guestbook. Please sign in.';
+    $lang->current_password = 'Current Password';
+    $lang->password1 = 'New Password';
+    $lang->password2 = 'Confirm new password';
+    $lang->about_change_password = 'Input new password again to check correctness.';
+    $lang->name_nick = 'Name(Nickname)';
+    $lang->manage_trackback ='Trackbacks';
+    $lang->manage_guestbook ='Guestbook';
+    $lang->manage_comment ='Comments';
+    $lang->document_list ='Document List';
+    $lang->type = 'Type';
+    $lang->trackback_site = 'Trackback Source';
+    $lang->total_result_count = 'There exist <strong>%d</strong> results.';
+    $lang->search_result_count = '<strong>%d</strong> results are found.';
+    $lang->no_result_count = 'There is no result';
+    $lang->selected_articles = 'Selected articles';
+    $lang->avatar = 'Avatar';
+    $lang->status = 'Status';
+    $lang->pingback = 'Pingback';
+    $lang->recent_tags = 'Recently used tags';
+    $lang->total_tag_result = 'Total <strong>%d</strong> tags are used.';
+    $lang->align_by_char = 'Alphabetical order';
+    $lang->used_documents = 'Number of Occurrences';
+    $lang->order_desc = 'Descending order';
+    $lang->update_tag = 'Tag modify/delete';
+    $lang->tag_name = 'Tag';
+    $lang->tag_with_tags = 'Co-occurred Tags';
+    $lang->total_materials = 'Total <strong>%d</strong> ingredients are stored.';
+    $lang->none_materials = 'There is no ingredient.';
+    $lang->install_bookmarklet = 'Install Ingredient Collector!.';
+    $lang->none_tags = 'There is no tag.';
+    $lang->bookmarklet_install = 'Install Bookmarklet';
+	$lang->about_bookmarklet = 'Collect ingredients to blog always.';
+    $lang->about_set_bookmarklet = 'Ingredients Collector';
+    $lang->data_export = 'Data Backup';
+    $lang->data_import = 'Data Migration (Import)';
+    $lang->migration_prepare = 'Analyzing files';
+    $lang->data_import_progress = 'Import progress rate';
+    $lang->data_export_progress = 'Export progress rate';
+    $lang->about_export_xexml = 'You can import data with XE XML format used in XpressEngine';
+    $lang->about_export_ttxml = 'You can import data with TTXML format used in Textcube (or Tistory)';
+    $lang->migration_file_path = 'Location of XML file(URL or path)';
+    $lang->msg_migration_file_is_null = 'Input the location of XML file for data import';
+    $lang->cmd_import = 'Import';
+    $lang->send_me2 = 'Publish to Me2Day';
+    $lang->about_send_me2 = 'Writing a post, publish the title to Me2Day.';
+    $lang->me2day_userid = 'Me2Day ID';
+    $lang->about_me2day_userid = 'Input ID for you Me2Day "http://me2day.net/ID".';
+    $lang->me2day_userkey = 'Me2Day UserKey';
+    $lang->about_me2day_userkey = 'Input user key appeared in the configuration page of Me2Day.';
+    $lang->check_me2day_info = 'Check Connection';
+    $lang->msg_success_to_me2day = 'The information you input is correct.';
+    $lang->msg_fail_to_me2day = 'Connection to Me2Day failed. Check ID and User Key';
+
+    $lang->send_twitter = 'Publish to Twitter';
+    $lang->about_send_twitter = 'Writing a post, publish the title to Twitter';
+    $lang->twitter_userid = 'Twitter ID';
+    $lang->about_twitter_userid = 'Input Twitter ID';
+    $lang->twitter_password = 'Twitter Password';
+    $lang->about_twitter_password = 'Input Twitter Password';
+
+    $lang->blogapi_publish = 'BlogAPI';
+    $lang->about_blog_api = 'Using BlogAPI, you can publish/ modify/ delete posts to other blogs or bulletin boards <BR /> Currently, Textyle only supports MetaWebLog API, but other APIs will be supported soon.';
+    $lang->cmd_registration_blogapi = 'Register BlogAPI Site';
+    $lang->cmd_modification_blogapi = 'Modify BlogAPI Site Information';
+    $lang->blogapi_site_url = 'API Site URL';
+    $lang->about_blogapi_site_url = 'Input URL for BlogAPI';
+    $lang->blogapi_site_title = 'Site\'s Title';
+    $lang->about_blogapi_site_title = 'You may set the title for BlogAPI';
+    $lang->blogapi_api_url = 'API URL';
+    $lang->about_blogapi_url = 'Input BlogAPI URL.';
+    $lang->blogapi_published = 'Number of Published Posts';
+    $lang->blogapi_user_id = 'UserID';
+    $lang->about_blogapi_user_id = 'Input user ID used in target site of API';
+    $lang->blogapi_password = 'Password';
+    $lang->about_blogapi_password = 'Input password used in target site of API';
+    $lang->cmd_get_site_info = 'Get Site Info.';
+    $lang->cmd_check_api_connect = 'Check API connection';
+    $lang->msg_url_is_invalid = "Cannot access to the URL\n\nCheck if URL is correct ";
+    $lang->msg_remove_api = 'Are you sure to delete API Information?';
+    $lang->msg_blogapi_registration = array(
+        'Input target site\'s address',
+        'Input target site\'s title',
+        'Input API URL',
+        'Input ID',
+        'Input password',
+    );
+
+    $lang->about_use_bookmarklet = 'How to Use Bookmarklet';
+    $lang->about_use_bookmarklet_item = array(
+        'Add "Ingredients Collector" bookmarklet to your browser',
+        'When you find resource to use for your writing, click the installed bookmarklet',
+        'Choose proper type (e.g., text, image, movie), edit it and save',
+        'Then, those ingredients will be stored in this page'
+    );
+    $lang->basket_management = 'Trashcan Mangement';
+    $lang->basket_list = '휴지통 보관 목록';
+    $lang->basket_empty = '휴지통이 깨끗 하네요. ^^;';
+
+    $lang->document_all = 'View All';
+    $lang->document_published = 'View Published';
+    $lang->document_reserved = 'View Temporarily Saved';
+
+    $lang->my_document_management = 'My Post management';
+    $lang->set_publish = 'Publish Configuration';
+    $lang->document_open = 'Open';
+    $lang->document_close = 'Closed';
+
+    $lang->category = 'Category';
+    $lang->comm_management = 'Communication';
+    $lang->allow_comment = 'Allow Comments';
+    $lang->allow_trackback = 'Allow Trackbacks';
+    $lang->publish_date = 'When to publish';
+    $lang->publish_now = 'Now';
+    $lang->publish_reserve = 'Reservation';
+    $lang->ymd = 'Year.Month.Day';
+    $lang->calendar = 'Calendar';
+    $lang->close_calendar_layer = 'Close calendar';
+    $lang->select_calendar_layer = 'Select date from calendar';
+
+    $lang->insert_title = 'Input title';
+    $lang->new_post = 'Write a Post';
+    $lang->modify_post = 'Modify a post';
+    $lang->posting_option = 'Posting Options';
+    $lang->post_url = 'Url for the post';
+    $lang->about_tag = 'Multiple tags are separated by comma(,)';
+	$lang->success_temp_saved = 'Temporary save succeeded.';
+
+    $lang->daily = 'Daily';
+    $lang->weekly = 'Weekly';
+    $lang->monthly = 'Monthly';
+    $lang->before_day = 'Yesterday';
+    $lang->after_day = 'Tomorrow';
+    $lang->before_month = 'Last Month';
+    $lang->after_month = 'Next Month';
+    $lang->this_month = 'This Month';
+    $lang->today = 'Today';
+    $lang->day_current = 'Today';
+    $lang->day_before = 'Yesterday';
+    $lang->about_status = array(
+        'day'=>'You can see the number of visitors by hours.',
+        'week'=>'You can see the number of visitors by day over a week.',
+        'month'=>'You can see the number of visitors by month over a year.',
+    );
+    $lang->about_unit = array(
+        'day'=>'Hour',
+        'week'=>'Year.Month.Day',
+        'month'=>'Year.Month'
+    );
+    $lang->visit_count = '# of Visitors';
+    $lang->visit_per = 'Proportion';
+    $lang->trackback_division = 'Multiple trackback urls are separated by newline(Enter)';
+
+    $lang->about_supporter = 'Supporters mean users who wrote comments, trackbacks, and guestbooks.';
+    $lang->supporter_rank = '지지자 순위';
+    $lang->rank = 'Rank';
+    $lang->user = 'User';
+    $lang->guestbook = 'Guestbook';
+	$lang->add_denylist = 'Add it to denied list';
+    $lang->summary = 'Summary';
+    $lang->no_supporter = 'There is no supporter.';
+    $lang->about_popular = 'Popular contents mean posts which have many views, comments, pingbacks and trackbacks.';
+    $lang->popular_rank = '인기 콘텐츠 순위';
+    $lang->read = 'View';
+    $lang->no_popular = 'There is no popular content.';
+    $lang->resize_vertical = 'Resize input form';
+
+    $lang->textyle_first_menus = array(
+        array('dispTextyleToolDashboard','Dashboard'),
+        array('','My Posts'),
+        array('','Communication'),
+        array('','Statistics'),
+        array('','Design'),
+        array('','Configuration'),
+    );
+
+    $lang->textyle_second_menus = array(
+        array(),
+        array(
+            'dispTextyleToolPostManageWrite'=>'Write a Post',
+            'dispTextyleToolPostManageList'=>'Posts',
+            'dispTextyleToolPostManageDeposit'=>'Writing Ingredients',
+            'dispTextyleToolPostManageCategory'=>'Category',
+            'dispTextyleToolPostManageTag'=>'Tags',
+            'dispTextyleToolPostManageBasket'=>'Trashcan',
+        ),
+        array(
+            'dispTextyleToolCommunicationComment'=>'Comments',
+            'dispTextyleToolCommunicationGuestbook'=>'Guestbook',
+            'dispTextyleToolCommunicationTrackback'=>'Trackbacks',
+            'dispTextyleToolCommunicationSpam'=>'Blocking Spams',
+        ),
+        array(
+            'dispTextyleToolStatisticsVisitor'=>'Visitor',
+            'dispTextyleToolStatisticsVisitRoute'=>'Referer',
+            'dispTextyleToolStatisticsSupporter'=>'Supporter',
+            'dispTextyleToolStatisticsPopular'=>'Popular Contents',
+        ),
+        array(
+            'dispTextyleToolLayoutConfigSkin'=>'Choose Skin',
+            'dispTextyleToolLayoutConfigEdit'=>'HTML&middot;CSS Edit',
+        ),
+        array(
+            'dispTextyleToolConfigProfile'=>'My Profile',
+            'dispTextyleToolConfigInfo'=>'Blog Configuration',
+            'dispTextyleToolConfigPostwrite'=>'Writing Env.',
+            'dispTextyleToolConfigCommunication'=>'Publish&middot;Comm.',
+            'dispTextyleToolConfigBlogApi'=>'BlogAPI',
+            'dispTextyleToolConfigAddon'=>'Addons',
+            'dispTextyleToolConfigData'=>'Data',
+            'dispTextyleToolConfigChangePassword'=>'Change Password',
+        ),
+    );
+
+    $lang->cmd_go_help = 'Go to HelpDesk';
+    $lang->cmd_textyle_setup = '기본 설정';
+    $lang->cmd_textyle_list = 'Textyle List';
+    $lang->cmd_textyle_creation = 'Create a Textyle';
+    $lang->cmd_textyle_update = 'Modify a Textyle';
+    $lang->cmd_new_post = 'Write a Post';
+    $lang->cmd_go_blog = 'Go to my blog';
+    $lang->cmd_send_suggestion = 'Send a suggestion';
+    $lang->cmd_view_help = 'View Help';
+    $lang->cmd_folding_menu = 'Fold/Open Menu';
+    $lang->cmd_folding_xe_news = 'Open/Close News';
+    $lang->cmd_apply = 'Apply';
+    $lang->cmd_delete_favicon = 'Remove favicon';
+    $lang->cmd_change_password = 'Change Password';
+    $lang->cmd_deny = 'Deny';
+    $lang->cmd_management = 'Management';
+    $lang->cmd_reply_comment = 'Reply to a comment';
+    $lang->cmd_change_secret = 'Make it secret';
+    $lang->cmd_write_relation = 'Write a related post';
+    $lang->cmd_delete_materials = 'Delete an ingredient';
+    $lang->cmd_restore = 'Restore';
+    $lang->cmd_empty = 'Empty';
+    $lang->cmd_empty_basket = 'Empty trash';
+    $lang->cmd_change_category = 'Change Category';
+    $lang->cmd_publish = 'Publish';
+    $lang->cmd_save_temp = 'Temporarily save';
+    $lang->cmd_edit_htmlcss = 'HTML&middot;CSS Edit';
+    $lang->cmd_edit_html = 'HTML Edit';
+    $lang->cmd_edit_css = 'CSS Edit';
+    $lang->cmd_use_ing = 'Using';
+    $lang->cmd_new_window = 'New window';
+    $lang->cmd_select_skin = 'Apply it';
+    $lang->msg_select_skin = 'Change Textyle skin into what you choose.\n\nCurrent configuration of skin will be removed.\n\nAre you sure to change it?';
+
+    $lang->cmd_preview_skin = 'Preview';
+    $lang->cmd_generate_widget_code = 'Generate Widget Code';
+
+    $lang->msg_already_used_url = 'The URL is already being used.';
+    $lang->alert_reset_skin = 'If you reset skin, all the HTML&middot;CSS you inputted will be removed.\n\nAre you sure to reset it?';
+
+	$lang->msg_close_before_write = "Changed contents are not saved.";
+
+    $lang->no_post = 'You have not written a post, <a href="%s">Write a Post!</a>';
+    $lang->no_trash = 'There is no trash in your trashcan!';
+    $lang->no_comment = 'There is no comment';
+    $lang->no_guestbook = 'No one left a message in your guestbook';
+    $lang->no_trackback = 'There is no trackback';
+
+    // service
+    $lang->view_all = 'View All';
+    $lang->search_result = 'Search Results';
+    $lang->category_result = 'Category';
+    $lang->newest_document = 'Recent Posts';
+    $lang->newest_comment = 'Recent Comments';
+    $lang->newest_trackback = 'Recent Trackbacks';
+    $lang->archive = 'Archive';
+    $lang->link = 'Links';
+
+    $lang->mail = 'Mail';
+    $lang->ip = 'IP';
+
+	$lang->secret_comment = 'Secret Comment';
+	$lang->insert_comment = 'Insert a comment';
+	$lang->content_list = 'Content List';
+	$lang->msg_input_email_address = 'Input your email address';
+	$lang->msg_input_homepage = 'Input your homepage address';
+	$lang->msg_confirm_delete_post = 'Temporary saved posts cannot be recovered. Are you sure to delete it?';
+
+    $lang->sample_title = 'Welcome to Textyle world!';
+    $lang->sample_tags = 'textyle ,  Textyle editor,  a writing ingredients collector';
+    $lang->msg_preparation = 'In preparation';
+?>
