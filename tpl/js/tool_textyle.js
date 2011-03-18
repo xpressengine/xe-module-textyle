2f2a086c49c4c096890e3ba4c4dc474e8845dcca
ChanMyeong <dece24@gmail.com>
2011-03-18 05:15:18 +0000

    label text clear bug fix & CSS code format.
    git-svn-id: http://xe-textyle.googlecode.com/svn/sandbox/textyle@526 bebf4043-ad85-6e1f-322f-7830e3d0fd93

--- tpl/js/tool_textyle.js
+++ tpl/js/tool_textyle.js
@@ -1243,3 +1243,28 @@ function sortExtraMenu(menu_mids){
 function isLive(){
 	exec_xml('textyle', 'procTextyleToolLive', []);
 }
+
+jQuery(function($){
+	// Label Text Clear
+	var iText = $('.fItem>.iLabel').next('.iText');
+	$('.fItem>.iLabel').css('position','absolute');
+	iText
+		.focus(function(){
+			$(this).prev('.iLabel').css('visibility','hidden');
+		})
+		.blur(function(){
+			if($(this).val() == ''){
+				$(this).prev('.iLabel').css('visibility','visible');
+			} else {
+				$(this).prev('.iLabel').css('visibility','hidden');
+			}
+		})
+		.change(function(){
+			if($(this).val() == ''){
+				$(this).prev('.iLabel').css('visibility','visible');
+			} else {
+				$(this).prev('.iLabel').css('visibility','hidden');
+			}
+		})
+		.blur();
+});
